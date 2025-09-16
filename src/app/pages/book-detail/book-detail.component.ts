41554bc87e2f918faa1d64a0e339fd567dbcc7ac
nnamdn29 <quocnam.dn29@gmail.com>
2025-09-16 23:22:14 +0700

    Enhance character and house detail pages with improved navigation and styling

--- src/app/pages/book-detail/book-detail.component.ts
+++ src/app/pages/book-detail/book-detail.component.ts
@@ -1,11 +1,10 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { AsyncPipe, DatePipe } from '@angular/common';
 
 import { forkJoin, map, Observable, switchMap, tap } from 'rxjs';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
-
 import { MatButtonModule } from '@angular/material/button';
 
 import { BookApi, CharacterApi } from '@app-shared/apis';
@@ -23,26 +22,27 @@ export class BookDetailComponent implements OnInit {
   private readonly route = inject(ActivatedRoute);
   private readonly bookApi = inject(BookApi);
   private readonly characterApi = inject(CharacterApi);
-  private readonly cdr = inject(ChangeDetectorRef);
   public book$!: Observable<Book>;
   public keyCharacters$!: Observable<Character[]>;
   public povCharacters$!: Observable<Character[]>;
 
   public ngOnInit(): void {
     this.book$ = this.route.params.pipe(
-      map((params) => params['bookId']),
-      switchMap((bookId: string) => this.bookApi.getBookById(bookId)),
+      switchMap((params) => this.bookApi.getBookById(`${params['bookId']}`)),
       tap((book) => {
-        const povCharacters = book.povCharacters.map((charUrl) =>
-          this.characterApi.getCharacterById(+StringUtil.getResourceIdFromUrl(charUrl))
-        );
-        const keyCharacters = book.characters
-          .slice(0, 3)
-          .map((charUrl) => this.characterApi.getCharacterById(+StringUtil.getResourceIdFromUrl(charUrl)));
-
-        this.povCharacters$ = forkJoin(povCharacters);
-        this.keyCharacters$ = forkJoin(keyCharacters);
+        const povCharacters = book.povCharacters.map(this.mapCharacterById);
+        const keyCharacters = book.characters.slice(0, 3).map(this.mapCharacterById);
+        this.povCharacters$ = forkJoin(povCharacters).pipe(map(this.mapCharacterUrl));
+        this.keyCharacters$ = forkJoin(keyCharacters).pipe(map(this.mapCharacterUrl));
       })
     );
   }
+
+  private mapCharacterUrl(chars: Character[]): Character[] {
+    return chars.map((char) => ({ ...char, url: StringUtil.getResourceIdFromUrl(char.url) }));
+  }
+
+  private readonly mapCharacterById = (url: string): Observable<Character> => {
+    return this.characterApi.getCharacterById(+StringUtil.getResourceIdFromUrl(url));
+  };
 }
