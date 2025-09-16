2e7a6c4b3f5a313e8bfddddc93e3eabff01bf98e
namnguyen29 <91844976+namnguyen29@users.noreply.github.com>
2025-09-16 23:22:58 +0700

    Enhance character and house detail pages with improved navigation and styling (#8)

--- src/app/pages/character-detail/character-detail.component.ts
+++ src/app/pages/character-detail/character-detail.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
+
+import { Observable, switchMap } from 'rxjs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
+import { CharacterApi } from '@app-shared/apis';
+import { Character } from '@app-shared/models';
 
 @Component({
   selector: 'app-character-detail',
-  imports: [],
+  imports: [MatIconModule, AsyncPipe, RouterLink, MatCardModule, MatButtonModule, MatBadgeModule],
   templateUrl: './character-detail.component.html',
-  styleUrl: './character-detail.component.scss'
+  styleUrl: './character-detail.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CharacterDetailComponent {
+export class CharacterDetailComponent implements OnInit {
+  private readonly route = inject(ActivatedRoute);
+  private readonly characterApi = inject(CharacterApi);
+  public character$!: Observable<Character>;
 
+  public ngOnInit(): void {
+    this.character$ = this.route.params.pipe(
+      switchMap((params) => {
+        const characterId = +params['characterId'];
+        return this.characterApi.getCharacterById(characterId);
+      })
+    );
+  }
 }
